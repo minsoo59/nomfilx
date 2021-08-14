3633f4e11990adc70ca7545a4d2d47df811f2e81
shushu <minsoo59@naver.com>
2021-08-14 17:48:49 +0900

    ok

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -14,7 +14,7 @@ const globalStyles = createGlobalStyle`
         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         font-size:12px;
         background-color:rgba(20, 20, 20, 1);
-        color:gray;
+        color:white;
         padding-top:80px;
     }
 `;
