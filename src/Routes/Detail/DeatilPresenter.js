543d69262fa696433b0bf0347549310f40665155
shushu <minsoo59@naver.com>
2021-08-19 17:34:02 +0900

    link undefied

--- src/Routes/Detail/DeatilPresenter.js
+++ src/Routes/Detail/DeatilPresenter.js
@@ -4,7 +4,6 @@ import styled from "styled-components";
 import Helmet from "react-helmet";
 import Loader from "Components/Loader";
 import Message from "Components/Message";
-import { Link } from "react-router-dom";
 
 const Container = styled.div`
   height: calc(100vh - 50px);
